alanwoolley/corsix-th

commit 33d0af8e71c726d3413072f4b5a433576e9e0ad2

Work in progress map editor (need to fix setting of hospital / buildable flag on tiles, and implement painting of plots - hopefully get done tomorrow). Add saving support to THMap class. Added luaT_execute as a simple way to run Lua snippets from C. Fix entrance doors to support being removed from a map. Fix game_ui.lua to be executable standalone.
git-svn-id: http://corsix-th.googlecode.com/svn/trunk@1009 c39591fa-788f-11de-a72b-d90af8dea425

CorsixTH/Lua/game_ui.lua
@@ -18,6 +18,8 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE. --]]
 
+dofile "ui"
+
 --! Variant of UI for running games
 class "GameUI" (UI)
 
@@ -56,11 +58,11 @@ function GameUI:GameUI(app, local_hospital)
   end
   self.screen_offset_x, self.screen_offset_y = app.map:WorldToScreen(
     app.map.th:getCameraTile(local_hospital:getPlayerIndex()))
+  self.zoom_factor = 1
   self:scrollMap(-scr_w / 2, 16 - scr_h / 2)
   self.limit_to_visible_diamond = not _MAP_EDITOR
   self.transparent_walls = false
   self.prevent_edge_scrolling = false
-  self.zoom_factor = 1
 end
 
 function GameUI:setZoom(factor)
@@ -194,7 +196,7 @@ function GameUI:onKeyDown(code, rawchar)
       local types = {"emergency", "epidemy", "personnality", "information", "disease", "report"}
       self.bottom_panel:queueMessage(types[math.random(1, #types)], message)
     elseif key == "f9" then -- Make debug patient
-      self:addWindow(UIMakeDebugPatient(self))
+      self.app.world:makeDebugPatient()
     
     elseif key == "f11" then -- Make Adviser say a random phrase
       self:debugMakeAdviserTalk()
@@ -571,7 +573,9 @@ function GameUI:makeWallsTransparent(mode)
   self.app.map.th:setWallDrawFlags(mode and 4 or 0)
 end
 
-local tutorial_phases = {
+local tutorial_phases
+local function make_tutorial_phases()
+tutorial_phases = {
   {
     -- 1) build reception
     { text = _S.adviser.tutorial.build_reception,      -- 1
@@ -685,6 +689,11 @@ local tutorial_phases = {
     },
   },
 }
+end
+tutorial_phases = setmetatable({}, {__index = function(t, k)
+  make_tutorial_phases()
+  return t[k]
+end})
 
 -- Called to trigger step to another part of the tutorial.
 -- chapter:    Individual parts of the tutorial. Step will only happen if it's the current chapter.

CorsixTH/Lua/objects/doors/entrance_right_door.lua
@@ -105,15 +105,17 @@ function EntranceDoor:setTile(x, y)
     end
   end
   Object.setTile(self, x, y)
-  if self.is_master then
-    self.world:notifyObjectOfOccupants(x , y , self)
-    for _, offset in ipairs(additional_walkable_tiles[self.direction]) do
-      self.world:notifyObjectOfOccupants(x + offset[1], y + offset[2], self)
+  if x then
+    if self.is_master then
+      self.world:notifyObjectOfOccupants(x, y, self)
+      for _, offset in ipairs(additional_walkable_tiles[self.direction]) do
+        self.world:notifyObjectOfOccupants(x + offset[1], y + offset[2], self)
+      end
     end
-  end
-  self.world.map:setCellFlags(x, y, {[flag_name] = true, buildable = false, doNotIdle = true})
-  for _, offset in ipairs(offsets) do
-    self.world.map:setCellFlags(x + offset[1], y + offset[2], {buildable = false, doNotIdle = true})
+    self.world.map:setCellFlags(x, y, {[flag_name] = true, buildable = false, doNotIdle = true})
+    for _, offset in ipairs(offsets) do
+      self.world.map:setCellFlags(x + offset[1], y + offset[2], {buildable = false, doNotIdle = true})
+    end
   end
   self.world.map.th:updateShadows()
 end

CorsixTH/Src/main.cpp
@@ -107,15 +107,10 @@ int CorsixTH_lua_main_no_eval(lua_State *L)
         lua_pop(L, 1);
     }
 
-    // package.preload.lfs = luaopen_lfs
-    lua_getglobal(L, "package");
-    lua_getfield(L, -1, "preload");
-
+    // Fill in package.preload table so that calls to require("X") from Lua
+    // will call the appropriate luaopen_X function in C.
 #define PRELOAD(name, fn) \
-    lua_pushliteral(L, name); \
-    luaT_pushcfunction(L, fn); \
-    lua_settable(L, -3)
-
+    luaT_execute(L, "package.preload." name " = ...", fn)
     PRELOAD("lfs", luaopen_lfs_ext);
     PRELOAD("lpeg", luaopen_lpeg);
     PRELOAD("rnc", luaopen_rnc);
@@ -123,14 +118,10 @@ int CorsixTH_lua_main_no_eval(lua_State *L)
     PRELOAD("ISO_FS", luaopen_iso_fs);
     PRELOAD("persist", luaopen_persist);
     PRELOAD("sdl", luaopen_sdl);
-    
 #undef PRELOAD
-    lua_pop(L, 2);
 
     // require "debug" (Harmless in Lua 5.1, useful in 5.2 for compatbility)
-    lua_getglobal(L, "require");
-    lua_pushliteral(L, "debug");
-    lua_call(L, 1, 0);
+    luaT_execute(L, "require \"debug\"");
 
     // Check for --interpreter and run that instead of CorsixTH.lua
     bool bGotScriptFile = false;

CorsixTH/Src/th_lua.cpp
@@ -319,3 +319,41 @@ int luaopen_th(lua_State *L)
     return 1;
 }
 
+void luaT_execute_loadstring(lua_State *L, const char* sLuaString)
+{
+    static const int iRegistryCacheIndex = 7;
+    lua_rawgeti(L, LUA_REGISTRYINDEX, iRegistryCacheIndex);
+    if(lua_isnil(L, -1))
+    {
+        // Cache not yet created - create it.
+        lua_pop(L, 1);
+        lua_getglobal(L, "setmetatable");
+        if(lua_isnil(L, -1))
+        {
+            // Base library not yet loaded - fallback to simple
+            // uncached loadstring
+            lua_pop(L, 1);
+            if(luaL_loadstring(L, sLuaString))
+                lua_error(L);
+        }
+        lua_pop(L, 1);
+        luaL_loadstring(L, "local assert, loadstring = assert, loadstring\n"
+            "return setmetatable({}, {__mode = [[v]], \n"
+            "__index = function(t, k)\n"
+                "local v = assert(loadstring(k))\n"
+                "t[k] = v\n"
+                "return v\n"
+            "end})");
+        lua_call(L, 0, 1);
+        lua_pushvalue(L, -1);
+        lua_rawseti(L, LUA_REGISTRYINDEX, iRegistryCacheIndex);
+    }
+    lua_getfield(L, -1, sLuaString);
+    lua_replace(L, -2);
+}
+
+void luaT_execute(lua_State *L, const char* sLuaString)
+{
+    luaT_execute_loadstring(L, sLuaString);
+    lua_call(L, 0, LUA_MULTRET);
+}

CorsixTH/Src/th_lua.h
@@ -83,7 +83,16 @@ const char* luaT_checkstring(lua_State *L, int idx, size_t* pLength);
 //! Push a C closure as a callable table
 void luaT_pushcclosuretable(lua_State *L, lua_CFunction fn, int n);
 
+//! Set a field on the environment table of a value
+/*!
+    Performs: env(stack[index])[k] = top; pop()
+*/
 void luaT_setenvfield(lua_State *L, int index, const char *k);
+
+//! Get a field from the environment table of a value
+/*!
+    Performs: push(env(stack[index])[k])
+*/
 void luaT_getenvfield(lua_State *L, int index, const char *k);
 
 template <class T>
@@ -236,6 +245,56 @@ static int luaT_stdgc(lua_State *L)
         p->~T();
     }
     return 0;
+}
+
+void luaT_execute(lua_State *L, const char* sLuaString);
+void luaT_execute_loadstring(lua_State *L, const char* sLuaString);
+
+template <class T> static void luaT_push(lua_State *L, T arg);
+
+template <> static void luaT_push(lua_State *L, lua_CFunction f)
+{
+    luaT_pushcfunction(L, f);
+}
+
+template <> static void luaT_push(lua_State *L, int i)
+{
+    lua_pushinteger(L, (lua_Integer)i);
+}
+
+template <> static void luaT_push(lua_State *L, const char* s)
+{
+    lua_pushstring(L, s);
+}
+
+template <class T>
+static void luaT_execute(lua_State *L, const char* sLuaString, T arg)
+{
+    luaT_execute_loadstring(L, sLuaString);
+    luaT_push(L, arg);
+    lua_call(L, 1, LUA_MULTRET);
+}
+
+template <class T1, class T2>
+static void luaT_execute(lua_State *L, const char* sLuaString,
+                         T1 arg1, T2 arg2)
+{
+    luaT_execute_loadstring(L, sLuaString);
+    luaT_push(L, arg1);
+    luaT_push(L, arg2);
+    lua_call(L, 2, LUA_MULTRET);
+}
+
+template <class T1, class T2, class T3, class T4>
+static void luaT_execute(lua_State *L, const char* sLuaString,
+                         T1 arg1, T2 arg2, T3 arg3, T4 arg4)
+{
+    luaT_execute_loadstring(L, sLuaString);
+    luaT_push(L, arg1);
+    luaT_push(L, arg2);
+    luaT_push(L, arg3);
+    luaT_push(L, arg4);
+    lua_call(L, 4, LUA_MULTRET);
 }
 
 #endif // CORSIX_TH_TH_LUA_H_

CorsixTH/Src/th_map.cpp
@@ -132,6 +132,13 @@ void THMap::_readTileIndex(const unsigned char* pData, int& iX, int &iY) const
     iY = iIndex / m_iWidth;
 }
 
+void THMap::_writeTileIndex(unsigned char* pData, int iX, int iY) const
+{
+    unsigned int iIndex = iY * m_iWidth + iX;
+    pData[0] = iIndex & 0xFF;
+    pData[1] = iIndex >> 8;
+}
+
 bool THMap::loadBlank()
 {
     if(!setSize(128, 128))
@@ -281,6 +288,88 @@ bool THMap::loadFromTHFile(const unsigned char* pData, size_t iDataLength,
         m_pParcelTileCounts[i] = _getParcelTileCount(i);
 
     return true;
+}
+
+void THMap::save(void (*fnWriter)(void*, const unsigned char*, size_t),
+              void* pToken)
+{
+    unsigned char aBuffer[256] = {0};
+    int iBufferNext = 0;
+
+    // Header
+    aBuffer[0] = (unsigned char)m_iPlayerCount;
+    // TODO: Determine correct contents for the next 33 bytes
+    fnWriter(pToken, aBuffer, 34);
+
+    unsigned char aReverseBlockLUT[256] = {0};
+    for(int i = 0; i < 256; ++i)
+    {
+        aReverseBlockLUT[gs_iTHMapBlockLUT[i]] = i;
+    }
+    aReverseBlockLUT[0] = 0;
+
+    for(THMapNode *pNode = m_pCells, *pLastNode = pNode + m_iWidth * m_iHeight;
+        pNode != pLastNode; ++pNode)
+    {
+        // TODO: Nicer system for saving object data
+        aBuffer[iBufferNext++] = 0;
+        aBuffer[iBufferNext++] = pNode->iFlags >> 24;
+
+        // Blocks
+        aBuffer[iBufferNext++] = aReverseBlockLUT[pNode->iBlock[0] & 0xFF];
+        aBuffer[iBufferNext++] = aReverseBlockLUT[pNode->iBlock[1] & 0xFF];
+        aBuffer[iBufferNext++] = aReverseBlockLUT[pNode->iBlock[2] & 0xFF];
+
+        // Flags (TODO: Set a few more flag bits?)
+        unsigned char iFlags = 3;
+        if(pNode->iFlags & THMN_Passable)
+            iFlags ^= 1;
+        if(pNode->iFlags & THMN_Buildable)
+            iFlags ^= 2;
+        aBuffer[iBufferNext++] = iFlags;
+        aBuffer[iBufferNext++] = 0;
+        iFlags = 16;
+        if(pNode->iFlags & THMN_Hospital)
+            iFlags ^= 16;
+        aBuffer[iBufferNext++] = iFlags;
+
+        if(iBufferNext == sizeof(aBuffer))
+        {
+            fnWriter(pToken, aBuffer, sizeof(aBuffer));
+            iBufferNext = 0;
+        }
+    }
+    for(THMapNode *pNode = m_pCells, *pLastNode = pNode + m_iWidth * m_iHeight;
+        pNode != pLastNode; ++pNode)
+    {
+        aBuffer[iBufferNext++] = pNode->iParcelId & 0xFF;
+        aBuffer[iBufferNext++] = pNode->iParcelId >> 8;
+        if(iBufferNext == sizeof(aBuffer))
+        {
+            fnWriter(pToken, aBuffer, sizeof(aBuffer));
+            iBufferNext = 0;
+        }
+    }
+
+    // TODO: What are these two bytes?
+    aBuffer[iBufferNext++] = 3;
+    aBuffer[iBufferNext++] = 0;
+    fnWriter(pToken, aBuffer, iBufferNext);
+    iBufferNext = 0;
+
+    memset(aBuffer, 0, 56);
+    for(int i = 0; i < m_iPlayerCount; ++i)
+    {
+        _writeTileIndex(aBuffer + iBufferNext,
+            m_aiInitialCameraX[i], m_aiInitialCameraY[i]);
+        _writeTileIndex(aBuffer + iBufferNext + 8,
+            m_aiHeliportX[i], m_aiHeliportY[i]);
+        iBufferNext += 2;
+    }
+    fnWriter(pToken, aBuffer, 16);
+    memset(aBuffer, 0, 16);
+    // TODO: What are these 56 bytes?
+    fnWriter(pToken, aBuffer, 56);
 }
 
 void THMap::setParcelOwner(int iParcelId, int iOwner)
@@ -463,6 +552,24 @@ bool THMap::getPlayerHeliportTile(int iPlayer, int* pX, int* pY) const
     if(pX) *pX = m_aiHeliportX[iPlayer];
     if(pY) *pY = m_aiHeliportY[iPlayer];
     return true;
+}
+
+void THMap::setPlayerCameraTile(int iPlayer, int iX, int iY)
+{
+    if(0 <= iPlayer && iPlayer < getPlayerCount())
+    {
+        m_aiInitialCameraX[iPlayer] = iX;
+        m_aiInitialCameraY[iPlayer] = iY;
+    }
+}
+
+void THMap::setPlayerHeliportTile(int iPlayer, int iX, int iY)
+{
+    if(0 <= iPlayer && iPlayer < getPlayerCount())
+    {
+        m_aiHeliportX[iPlayer] = iX;
+        m_aiHeliportY[iPlayer] = iY;
+    }
 }
 
 int THMap::getParcelTileCount(int iParcelId) const

CorsixTH/Src/th_map.h
@@ -104,6 +104,7 @@ enum THMapNodeFlags
     THMN_CanTravelS = 1 <<  3, //!< Pathfinding: Can walk to the south
     THMN_CanTravelW = 1 <<  4, //!< Pathfinding: Can walk to the west
     THMN_Hospital   = 1 <<  5, //!< World: Tile is inside a hospital building
+    THMN_Hospital_Shift = 5,
     THMN_Buildable  = 1 <<  6, //!< Player: Can build on this tile
     //! Pathfinding: Normally can walk on this tile, but can't due to blueprint
     THMN_PassableIfNotForBlueprint = 1 << 7,
@@ -189,6 +190,9 @@ public:
                         THMapLoadObjectCallback_t fnObjectCallback,
                         void* pCallbackToken);
 
+    void save(void (*fnWriter)(void*, const unsigned char*, size_t),
+              void* pToken);
+
     //! Set the sprite sheet to be used for drawing the map
     /*!
         The sprites for map floor tiles, wall tiles, and map decorators
@@ -220,6 +224,8 @@ public:
     inline int getPlayerCount() const {return m_iPlayerCount;}
     bool getPlayerCameraTile(int iPlayer, int* pX, int* pY) const;
     bool getPlayerHeliportTile(int iPlayer, int* pX, int* pY) const;
+    void setPlayerCameraTile(int iPlayer, int iX, int iY);
+    void setPlayerHeliportTile(int iPlayer, int iX, int iY);
 
     //! Get the number of tiles inside a given parcel
     int getParcelTileCount(int iParcelId) const;
@@ -321,6 +327,7 @@ protected:
     THDrawable* _hitTestDrawables(THLinkList* pListStart, int iXs, int iYs,
                                   int iTestX, int iTestY) const;
     void _readTileIndex(const unsigned char* pData, int& iX, int &iY) const;
+    void _writeTileIndex(unsigned char* pData, int iX, int iY) const;
     int _getParcelTileCount(int iParcelId) const;
 
     //! Create the adjacency matrix if it doesn't already exist

MapEdit/Src/embedded_game.cpp
@@ -34,6 +34,10 @@ BEGIN_EVENT_TABLE(EmbeddedGamePanel, wxGLCanvas)
   EVT_LEFT_DCLICK(EmbeddedGamePanel::_onLeftDoubleClick)
 END_EVENT_TABLE()
 
+IEmbeddedGamePanel::~IEmbeddedGamePanel()
+{
+}
+
 EmbeddedGamePanel::EmbeddedGamePanel(wxWindow *pParent)
   : wxGLCanvas(pParent, wxID_ANY, NULL, wxDefaultPosition,
         wxDefaultSize, 0, wxGLCanvasName, wxNullPalette)
@@ -67,19 +71,45 @@ void EmbeddedGamePanel::setLogWindow(frmLog *pLogWindow)
     m_pPrintTarget = pLogWindow->getTextControl();
 }
 
+THMap* EmbeddedGamePanel::getMap()
+{
+    THMap *pMap = NULL;
+    int iTop = lua_gettop(m_L);
+    lua_checkstack(m_L, 5);
+    lua_getglobal(m_L, "TheApp");
+    if(!lua_isnil(m_L, -1))
+    {
+        lua_getfield(m_L, -1, "map");
+        if(!lua_isnil(m_L, -1))
+        {
+            lua_getfield(m_L, -1, "th");
+            pMap = reinterpret_cast<THMap*>(lua_touserdata(m_L, -1));
+        }
+    }
+    lua_settop(m_L, iTop);
+    return pMap;
+}
+
 bool EmbeddedGamePanel::loadLua()
 {
+    // Create state
     m_L = luaL_newstate();
+    lua_atpanic(m_L, _l_panic);
+
+    // Save a pointer to ourselves in the registry
     lua_pushliteral(m_L, "wxWindow");
     lua_pushlightuserdata(m_L, reinterpret_cast<wxWindow*>(this));
     lua_settable(m_L, LUA_REGISTRYINDEX);
-    lua_pushliteral(m_L, "_MAP_EDITOR");
-    lua_pushboolean(m_L, 1);
-    lua_settable(m_L, LUA_GLOBALSINDEX);
-    lua_atpanic(m_L, _l_panic);
+
+    // Open default libraries, and override appropriate bits
     luaL_openlibs(m_L);
-    lua_pushcfunction(m_L, _l_print);
-    lua_setglobal(m_L, "print");
+    luaT_execute(m_L, "print = ...", _l_print);
+
+    // Set _MAP_EDITOR to true, to allow scripts to notice that they are
+    // running inside this component, rather than standalone.
+    luaT_execute(m_L, "_MAP_EDITOR = true");
+
+    // Load CorsixTH.lua and perform other initialisation needed by it
     lua_settop(m_L, 0);
     lua_pushcfunction(m_L, CorsixTH_lua_stacktrace);
     lua_pushcfunction(m_L, CorsixTH_lua_main_no_eval);
@@ -96,17 +126,16 @@ bool EmbeddedGamePanel::loadLua()
         }
         return false;
     }
-    // NB: THMain_l_main will have loaded CorsixTH.lua but not yet run it
+    // NB: CorsixTH_lua_main_no_eval will have loaded CorsixTH.lua and left it
+    // as the top value on the stack, but will not have executed it.
+    // The stack will hence have two things on it: the stacktrace function,
+    // and the loaded CorsixTH.lua
 
-    // package.preload.sdl = _l_open_sdl
-    lua_getglobal(m_L, "package");
-    lua_getfield(m_L, -1, "preload");
-    lua_pushliteral(m_L, "sdl");
-    lua_pushcfunction(m_L, _l_open_sdl);
-    lua_settable(m_L, -3);
-    lua_pop(m_L, 2);
+    // Overwrite what CorsixTH_lua_main_no_eval registered for require("sdl")
+    // with our own function that uses wxWidgets to do what SDL would have.
+    luaT_execute(m_L, "package.preload.sdl = ...", _l_open_sdl);
 
-    // require"TH".surface.endFrame = _l_end_frame
+    // Replace the Surface:endFrame() function with our own
     lua_getglobal(m_L, "require");
     lua_pushliteral(m_L, "TH");
     lua_call(m_L, 1, 1);
@@ -134,158 +163,115 @@ bool EmbeddedGamePanel::loadLua()
     {
         if(m_pPrintTarget)
         {
-            lua_getglobal(L, "debug");
-            lua_getfield(L, -1, "traceback");
-            lua_replace(L, -2);
-            lua_pushthread(L);
-            lua_getglobal(L, "tostring");
-            lua_pushvalue(L, -4);
-            lua_call(L, 1, 1);
-            lua_pushinteger(L, 1);
-            lua_call(L, 3, 1);
+            // Push debug.traceback onto m_L
+            lua_getglobal(m_L, "debug");
+            lua_getfield(m_L, -1, "traceback");
+            lua_replace(m_L, -2);
+            // Push the thread onto m_L
+            lua_pushvalue(m_L, -2);
+            // Push tostring(errmsg) onto m_L
+            lua_getglobal(m_L, "tostring");
+            lua_xmove(L, m_L, 1);
+            lua_call(m_L, 1, 1);
+            // Push constant 1 onto m_L
+            lua_pushinteger(m_L, 1);
+            // Call debug.traceback(thread, tostring(err), 1)
+            lua_call(m_L, 3, 1);
+            // Display resulting string and pop it
             m_pPrintTarget->AppendText(L"Error initialising Lua: ");
-            m_pPrintTarget->AppendText(lua_tostring(L, -1));
+            m_pPrintTarget->AppendText(lua_tostring(m_L, -1));
             m_pPrintTarget->AppendText(L"\n");
+            lua_pop(m_L, 1);
         }
         return false;
     }
-    lua_settop(L, 1); // the event coroutine
+    lua_settop(L, 1);
     m_Lthread = L;
+
+    // The stack of the Lua states is now as follows:
+    // m_L: stacktrace function, m_Lthread <top
+    // m_Lthread: event dispatch coroutine <top
 
     return true;
 }
 
 bool EmbeddedGamePanel::_resume(lua_State *L, int iNArgs, int iNRes)
 {
+    // L will be an event handling coroutine.
+    // Start by sending the event to the coroutine by resuming it with the
+    // event arguments.
     bool bGood = true;
     if(lua_resume(L, iNArgs) != LUA_YIELD)
     {
+        // Error occured during event processing.
         bGood = false;
+        // Transfer the error details to m_Lthread
         int iNTransfer = lua_gettop(L);
         if(lua_status(L) >= LUA_ERRRUN)
             iNTransfer = 1;
         lua_checkstack(m_Lthread, iNTransfer);
         lua_xmove(L, m_Lthread, iNTransfer);
         lua_settop(L, 0);
-        lua_resume(m_Lthread, 1);
+        // Allow m_Lthread to respond to the error in an appropriate way
+        lua_resume(m_Lthread, iNTransfer);
     }
     else
     {
+        // Event processed without errors, and will have returned true if
+        // a redraw needs to occur.
         if(lua_toboolean(L, -1) != 0)
         {
             Refresh(false);
         }
     }
+    // Leave L with the desired number of return values.
     lua_settop(L, iNRes);
     return bGood;
 }
 
 void EmbeddedGamePanel::_onMouseMove(wxMouseEvent& evt)
 {
-    if(m_Lthread)
-    {
-        lua_State *L = lua_tothread(m_Lthread, 1);
-        lua_pushliteral(L, "motion");
-        lua_pushinteger(L, evt.GetX());
-        lua_pushinteger(L, evt.GetY());
-        wxPoint ptNew = evt.GetPosition();
-        wxPoint ptDelta = ptNew - m_ptMouse;
-        m_ptMouse = ptNew;
-        lua_pushinteger(L, ptDelta.x);
-        lua_pushinteger(L, ptDelta.y);
-        _resume(L, 5, 0);
-    }
+    // Keep track of relative mouse movements as well as absolute
+    wxPoint ptNew = evt.GetPosition();
+    wxPoint ptDelta = ptNew - m_ptMouse;
+    m_ptMouse = ptNew;
+
+    _dispatchEvent("motion", ptNew.x, ptNew.y, ptDelta.x, ptDelta.y);
 }
 
 void EmbeddedGamePanel::_onLeftUp(wxMouseEvent& evt)
 {
-    if(m_Lthread)
-    {
-        lua_State *L = lua_tothread(m_Lthread, 1);
-        lua_pushliteral(L, "buttonup");
-        lua_pushinteger(L, SDL_BUTTON_LEFT);
-        lua_pushinteger(L, evt.GetX());
-        lua_pushinteger(L, evt.GetY());
-        _resume(L, 4, 0);
-    }
+    _dispatchEvent("buttonup", SDL_BUTTON_LEFT, evt.GetX(), evt.GetY());
 }
 
 void EmbeddedGamePanel::_onLeftDown(wxMouseEvent& evt)
 {
-    if(m_Lthread)
-    {
-        lua_State *L = lua_tothread(m_Lthread, 1);
-        lua_pushliteral(L, "buttondown");
-        lua_pushinteger(L, SDL_BUTTON_LEFT);
-        lua_pushinteger(L, evt.GetX());
-        lua_pushinteger(L, evt.GetY());
-        _resume(L, 4, 0);
-    }
+    _dispatchEvent("buttondown", SDL_BUTTON_LEFT, evt.GetX(), evt.GetY());
 }
 
 void EmbeddedGamePanel::_onLeftDoubleClick(wxMouseEvent& evt)
 {
-    if(m_Lthread)
-    {
-        lua_State *L = lua_tothread(m_Lthread, 1);
-        lua_pushliteral(L, "buttondown");
-        lua_pushliteral(L, "left_double");
-        lua_pushinteger(L, evt.GetX());
-        lua_pushinteger(L, evt.GetY());
-        _resume(L, 4, 0);
-    }
+    _dispatchEvent("buttondown", "left_double", evt.GetX(), evt.GetY());
 }
 
 void EmbeddedGamePanel::_onRightUp(wxMouseEvent& evt)
 {
-    if(m_Lthread)
-    {
-        lua_State *L = lua_tothread(m_Lthread, 1);
-        lua_pushliteral(L, "buttonup");
-        lua_pushinteger(L, SDL_BUTTON_RIGHT);
-        lua_pushinteger(L, evt.GetX());
-        lua_pushinteger(L, evt.GetY());
-        _resume(L, 4, 0);
-    }
+    _dispatchEvent("buttonup", SDL_BUTTON_RIGHT, evt.GetX(), evt.GetY());
 }
 
 void EmbeddedGamePanel::_onRightDown(wxMouseEvent& evt)
 {
-    if(m_Lthread)
-    {
-        lua_State *L = lua_tothread(m_Lthread, 1);
-        lua_pushliteral(L, "buttondown");
-        lua_pushinteger(L, SDL_BUTTON_RIGHT);
-        lua_pushinteger(L, evt.GetX());
-        lua_pushinteger(L, evt.GetY());
-        _resume(L, 4, 0);
-    }
+    _dispatchEvent("buttondown", SDL_BUTTON_RIGHT, evt.GetX(), evt.GetY());
 }
 
 void EmbeddedGamePanel::_onMiddleUp(wxMouseEvent& evt)
 {
-    if(m_Lthread)
-    {
-        lua_State *L = lua_tothread(m_Lthread, 1);
-        lua_pushliteral(L, "buttonup");
-        lua_pushinteger(L, SDL_BUTTON_MIDDLE);
-        lua_pushinteger(L, evt.GetX());
-        lua_pushinteger(L, evt.GetY());
-        _resume(L, 4, 0);
-    }
+    _dispatchEvent("buttonup", SDL_BUTTON_MIDDLE, evt.GetX(), evt.GetY());
 }
 
 void EmbeddedGamePanel::_onMiddleDown(wxMouseEvent& evt)
 {
-    if(m_Lthread)
-    {
-        lua_State *L = lua_tothread(m_Lthread, 1);
-        lua_pushliteral(L, "buttondown");
-        lua_pushinteger(L, SDL_BUTTON_MIDDLE);
-        lua_pushinteger(L, evt.GetX());
-        lua_pushinteger(L, evt.GetY());
-        _resume(L, 4, 0);
-    }
+    _dispatchEvent("buttondown", SDL_BUTTON_MIDDLE, evt.GetX(), evt.GetY());
 }
 
 void EmbeddedGamePanel::_onPaint(wxPaintEvent& evt)
@@ -344,13 +330,19 @@ void EmbeddedGamePanel::_onPaint(wxPaintEvent& evt)
     }
 }
 
-int EmbeddedGamePanel::_l_print(lua_State *L)
+EmbeddedGamePanel* EmbeddedGamePanel::_getThis(lua_State *L)
 {
     lua_pushliteral(L, "wxWindow");
     lua_gettable(L, LUA_REGISTRYINDEX);
     EmbeddedGamePanel *pThis = reinterpret_cast<EmbeddedGamePanel*>(
         reinterpret_cast<wxWindow*>(lua_touserdata(L, -1)));
     lua_pop(L, 1);
+    return pThis;
+}
+
+int EmbeddedGamePanel::_l_print(lua_State *L)
+{
+    EmbeddedGamePanel *pThis = _getThis(L);
     if(pThis->m_pPrintTarget)
     {
         int iCount = lua_gettop(L);
@@ -380,15 +372,16 @@ int EmbeddedGamePanel::_l_panic(lua_State *L)
 
 int EmbeddedGamePanel::_l_end_frame(lua_State *L)
 {
+    // Call the original Surface:endFrame() function
     lua_pushvalue(L, lua_upvalueindex(1));
     lua_insert(L, 1);
     lua_call(L, lua_gettop(L) - 1, LUA_MULTRET);
-    lua_pushliteral(L, "wxWindow");
-    lua_gettable(L, LUA_REGISTRYINDEX);
-    EmbeddedGamePanel *pThis = reinterpret_cast<EmbeddedGamePanel*>(
-        reinterpret_cast<wxWindow*>(lua_touserdata(L, -1)));
-    lua_pop(L, 1);
+
+    // Update the display
+    EmbeddedGamePanel *pThis = _getThis(L);
     pThis->m_pGLCanvas->SwapBuffers();
+
+    // Return whatever the original returned
     return lua_gettop(L);
 }
 

commit 13916a9d274b9f73dcbc37283a1de23347e03479

Fix tutorial being broken since r1009.
git-svn-id: http://corsix-th.googlecode.com/svn/trunk@1022 c39591fa-788f-11de-a72b-d90af8dea425

CorsixTH/Lua/game_ui.lua
@@ -691,7 +691,7 @@ tutorial_phases = {
 end
 tutorial_phases = setmetatable({}, {__index = function(t, k)
   make_tutorial_phases()
-  return t[k]
+  return tutorial_phases[k]
 end})
 
 -- Called to trigger step to another part of the tutorial.