1fcceac10ef32dcbf23261f9f7c9d8b4deeecfbb
Alan Woolley <alan@armedpineapple.co.uk>
2012-11-10 15:52:56 +0000

    Add timestamp to logs

--- CorsixTH/logging.cpp
+++ CorsixTH/logging.cpp
@@ -1,31 +1,43 @@
 #include "logging.h"
 #include <stdio.h>
+#include <time.h>
 
 FILE* fo;
 FILE* fe;
 
-void LOG_ERROR(std::string strtext) {
+void formatString(std::string *strtext) {
 
-	if (strtext.length() > 0) {
-		if (strtext.at((strtext.length() - 1)) != '\n') {
-			strtext.resize(strtext.length() + 1, '\n');
+	time_t current_time = time(NULL);
+	std::string timestring = ctime(&current_time);
+
+	std::string insertstring(timestring.substr(0,timestring.length()-1));
+
+	strtext->insert(0,insertstring + ": ");
+
+	if (strtext->length() > 0) {
+		if (strtext->at((strtext->length() - 1)) != '\n') {
+			strtext->resize(strtext->length() + 1, '\n');
 		}
 	}
 
+}
+
+void LOG_ERROR(std::string strtext) {
+
 	LOGW(strtext.c_str());
+	formatString(&strtext);
+
+
 	fprintf(stderr, strtext.c_str());
 
 }
 
 void LOG_INFO(std::string strtext) {
 
-	if (strtext.length() > 0) {
-		if (strtext.at((strtext.length() - 1)) != '\n') {
-			strtext.resize(strtext.length() + 1, '\n');
-		}
-	}
-
 	LOGI(strtext.c_str());
+	formatString(&strtext);
+
+
 	fprintf(stdout, strtext.c_str());
 
 }
@@ -35,8 +47,8 @@ void START_LOGGING(const char* root) {
 	std::string logfile = std::string(root) + "/cthlog.txt";
 	std::string errlogfile = std::string(root) + "/ctherrlog.txt";
 	// Redirect stdout and stderr
-	fo = freopen(logfile.c_str(), "w", stdout);
-	fe = freopen(errlogfile.c_str(), "w", stderr);
+	fo = freopen(logfile.c_str(), "a", stdout);
+	fe = freopen(errlogfile.c_str(), "a", stderr);
 
 	// Disable buffering so messages are written instantly
 	setvbuf(fo, 0, _IONBF, 0);
