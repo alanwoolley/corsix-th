5ca109c1da32e940efd28bc502e75afaeb22be43
Alan Woolley <alan@armedpineapple.co.uk>
2013-01-27 13:58:06 +0000

    Restore log formatting

--- CorsixTH/logging.cpp
+++ CorsixTH/logging.cpp
@@ -1,31 +1,29 @@
 #include "logging.h"
 
-
 FILE* fo;
 FILE* fe;
 
 void formatString(std::string *strtext) {
-/*
+
 	time_t current_time = time(NULL);
 	std::string timestring = ctime(&current_time);
 
-	std::string insertstring(timestring.substr(0,timestring.length()-1));
+	std::string insertstring(timestring.substr(0, timestring.length() - 1));
 
-	strtext->insert(0,insertstring + ": ");
+	strtext->insert(0, insertstring + ": ");
 
 	if (strtext->length() > 0) {
 		if (strtext->at((strtext->length() - 1)) != '\n') {
 			strtext->resize(strtext->length() + 1, '\n');
 		}
 	}
-*/
+
 }
 
 void LOG_ERROR(std::string strtext) {
 
 	LOGW(strtext.c_str());
 	formatString(&strtext);
-
 
 	fprintf(stderr, strtext.c_str());
 
@@ -36,7 +34,6 @@ void LOG_INFO(std::string strtext) {
 	LOGI(strtext.c_str());
 	formatString(&strtext);
 
-
 	fprintf(stdout, strtext.c_str());
 
 }
@@ -46,8 +43,8 @@ void START_LOGGING(const char* root) {
 	std::string logfile = std::string(root) + "/cthlog.txt";
 	std::string errlogfile = std::string(root) + "/ctherrlog.txt";
 	// Redirect stdout and stderr
-	fo = freopen(logfile.c_str(), "a", stdout);
-	fe = freopen(errlogfile.c_str(), "a", stderr);
+	fo = freopen(logfile.c_str(), "w", stdout);
+	fe = freopen(errlogfile.c_str(), "w", stderr);
 
 	// Disable buffering so messages are written instantly
 	setvbuf(fo, 0, _IONBF, 0);
